6464e87a2d0f937d00634fdeb5d4b9b1bf709257
Choi Inha <bella020207@naver.com>
2025-01-27 19:55:12 +0900

    2

--- birthday-page/src/index.js
+++ birthday-page/src/index.js
@@ -7,7 +7,9 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename={process.env.PUBLIC_URL}> {/* BrowserRouter로 App 감싸기 */}
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
